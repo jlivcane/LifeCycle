e51ca5d16d68f3e55b280343abbb99e56ecec1a6
jlivcane <kalibrik@inbox.lv>
2020-08-31 19:04:06 +0300

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by jekaterina.livcane on 31/08/2020.
+//  Copyright © 2020 jekaterina.livcane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
